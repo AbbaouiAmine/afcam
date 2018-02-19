d3b956d1bbade0351372bc7ca580b997772ad3b1
Abbaoui <abbaouiamine.r@gmail.com>
2018-02-19 06:21:10 -0800

    2/19/2018
    Version 0.2

---
+++ js/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('#menu a, .banner, h2, h3, h4, .button2, #footer_logo, .tel, .nav a', { fontFamily: 'Arial', hover:true });
+Cufon.replace('.pagination a', { fontFamily: 'Arial', hover:true, textShadow:'rgba(0, 0, 0, .1) 1px 1px' });
